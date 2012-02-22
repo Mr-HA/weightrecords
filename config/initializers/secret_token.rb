b1b0f9855ee5bafce78a0fc9cbc1773f649b3fe4
Mr-HA <mr.ha.facebook@gmail.com>
2012-02-22 18:01:54 +0800

    Ran the cucumber and rspec generators

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weightrecords::Application.config.secret_token = 'cdeb2ce4c2b74a00e209fa2fc25d9af37c1d24f1b5161b2d48dd8e26de80d1cc252ec211409466036d5fc5d47037ec3c1a24c31c06d68e8b4c0d3d0824e5a47f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Weightrecords::Application.config.session_store :cookie_store, key: '_weightrecords_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Weightrecords::Application.config.session_store :active_record_store
